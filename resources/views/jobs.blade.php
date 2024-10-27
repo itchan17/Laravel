d156b50a4e20b359b82a946f6f62626052d2129f
Christian Anoyo <xtianseventeen@gmail.com>
2024-10-27 12:08:33 +0800

    Change heading to listiiiing

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-slot:heading>Job Lists</x-slot:heading>
+    <x-slot:heading>Job Listiiiing</x-slot:heading>
     <ul>
         @foreach ($jobs as $job)
             <li class="hover:text-blue-500 hover:underline"><a href="/jobs/{{ $job['id'] }}">{{ $job['title'] }}:
